78eb35f95ea9d51d6e692ae3e928ca03b1489e91
Prakash15 <prakashkumarsingh483@gmail.com>
2025-05-19 19:51:02 +0530

    created folder structure, entities & schema

---
+++ src/main/java/com/Prakash/AirBnb/entity/enums/BookingStatus.java
@@ -0,0 +1,7 @@
+package com.Prakash.AirBnb.entity.enums;
+
+public enum BookingStatus {
+    RESERVED,
+    CONFIRMED,
+    CANCELLED
+}
---
+++ src/main/java/com/Prakash/AirBnb/entity/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.Prakash.AirBnb.entity.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
